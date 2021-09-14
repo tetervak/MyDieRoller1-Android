003ec3c137cd8e2cc3199387bc1f05d585afa0b7
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-09-14 14:11:55 -0400

    saved the instance state

--- app/src/main/java/sheridan/tetervak/mydieroller1/MainActivity.kt
+++ app/src/main/java/sheridan/tetervak/mydieroller1/MainActivity.kt
@@ -12,22 +12,42 @@ class MainActivity : AppCompatActivity() {
 
     companion object{
         const val TAG = "MainActivity"
+        const val RESULT = "Result"
+
     }
+
+    private var dieValue: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState is Bundle){
+            val savedResult: Int = savedInstanceState.getInt(RESULT)
+            if(savedResult > 0){
+                dieValue = savedResult
+                val rollResult: TextView = findViewById(R.id.roll_result)
+                rollResult.text = savedResult.toString()
+            }
+        }
+
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             Toast.makeText(this, "Rolled", Toast.LENGTH_LONG).show()
             Log.d(TAG, "The die is rolled")
 
-            val rand = (1..6).random()
+            dieValue = (1..6).random()
             val rollResult: TextView = findViewById(R.id.roll_result)
-            rollResult.text = rand.toString()
+            rollResult.text = dieValue.toString()
         }
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if(dieValue > 0){
+            outState.putInt(RESULT, dieValue)
+        }
 
     }
 }
